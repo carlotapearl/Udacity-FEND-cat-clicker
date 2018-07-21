dba000b90033c97b7a14a3c537a14153d9e6973e
Carlota Pearl <carlota.pearl@gmail.com>
2018-07-20 20:08:48 -0400

    Cat Clicker Version 2

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ A spec was given to implement.
 | Display Requirements | Interaction Requirements | Commit Url |
 | ------------- | ------------- | :-------------: |
 | The application should display a picture of a cat and a number of clicks. | The number of clicks should increment when the cat picture is clicked. | [Cat Clicker version 1](https://github.com/carlotapearl/Udacity-FEND-cat-clicker/tree/aa1702c813aff03ce502f8c3bbf6bd0278c28126)
+| The application should display two cats.<br /><br />Each cat includes: the cat's name, a picture of the cat, text showing the number of clicks. | The number of clicks should increment when each cat picture is clicked. | [Cat Clicker version 2](https://github.com/carlotapearl/Udacity-FEND-cat-clicker/)
--- cat-clicker.js
+++ cat-clicker.js
@@ -1,18 +1,60 @@
-//Project Requirements for Cat Clicker
-//Visuals
-//The application should display a picture of a cat and a number of clicks.
-//The specifics of the layout do not matter, so style it however you'd like.
-//Interaction
-//The number of clicks should increment when the cat picture is clicked.
+/*
+* Version 2 
+* New Project Requirements for Cat Clicker
+* Visuals
+* The application should display two cats. Each cat includes
+* the cat's name
+* a picture of the cat
+* text showing the number of clicks
+* The specifics of the layout do not matter, so style it however you'd like.
+* Interaction
+* The number of clicks should increment when each cat picture is clicked.
+*/
 
-const kittyImage = document.getElementById('kitty');
-const clickCount = document.getElementById('clicker');
-let count = 0;
+var cats = [];
+var Cat = function(name, img_url, default_click_count) {
+  this.name = name;
+  this.image = img_url;
+  this.clickCount = default_click_count;
+};
 
-function setCount(count) {
-    clickCount.innerText = count;
-}
-kittyImage.addEventListener('click', () => {
-  setCount(++count);
+Cat.prototype.createDisplay = function() {
+    var newDiv = document.createElement("div");
+    var newCatImg = new Image();
+    newCatImg.src = this.image;
+    newDiv.appendChild(newCatImg);
+    this.imgElement = newCatImg;
   
-}, false);
+    var newP = document.createElement("p");
+    newP.innerText = this.name + "'s Clicks: ";
+    var newCount = document.createElement("strong");
+    newCount.innerText = this.clickCount;
+    newP.appendChild(newCount);
+    newDiv.appendChild(newP);
+    this.countElement = newCount;
+  
+    catContainer.appendChild(newDiv);
+  };
+
+  Cat.prototype.bindImage = function() {
+    var cat = this;
+    cat.imgElement.addEventListener('click', function(){
+      cat.clickCount++;
+      cat.countElement.innerText = cat.clickCount;
+    }, false);
+  };
+  Cat.prototype.init = function() {
+    this.createDisplay();
+    this.bindImage();
+  };
+  
+  cats.push(new Cat("Silvester", "images/chewie.jpg", 0));
+  cats.push(new Cat("Milo", "images/poplinrecat.jpg", 0));
+  
+  var catContainer = document.getElementById("cat_container");
+  
+  var startCatClicker = function() {
+    for (var i = 0; i < cats.length; i++) {
+      cats[i].init();
+    }
+  }();
